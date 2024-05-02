85e3e2f06d7dff164d58308f69a528c24bec899b
phhoangloc <82487565+phhoangloc@users.noreply.github.com>
2024-05-02 13:48:20 +0900

    0502

--- next.config.mjs
+++ next.config.mjs
@@ -5,10 +5,10 @@ const nextConfig = {
     env: {
         MONGODB_URL: "mongodb+srv://h-loc:6pS3qvpeUQxuubKi@cluster0.kvimrln.mongodb.net/bulletin?retryWrites=true&w=majority",
         // MONGODB_URL: "mongodb://h-loc:6pS3qvpeUQxuubKi@ac-5djtpn4-shard-00-00.qs9wtoh.mongodb.net:27017,ac-5djtpn4-shard-00-01.qs9wtoh.mongodb.net:27017,ac-5djtpn4-shard-00-02.qs9wtoh.mongodb.net:27017/bulletin?ssl=true&replicaSet=atlas-d75adp-shard-0&authSource=admin&retryWrites=true&w=majority",
-        HOMEPAGE_URL_: "https://articlealert-lockhearts-projects.vercel.app/",
+        HOMEPAGE_URL: "https://articlealert-lockhearts-projects.vercel.app/",
         SOCKET_URL: "https://be-socket-mu.vercel.app/",
         SOCKET_URL_: "http://localhost:4000/",
-        HOMEPAGE_URL: "http://localhost:3000/"
+        HOMEPAGE_URL_: "http://localhost:3000/"
     }
 };
 
